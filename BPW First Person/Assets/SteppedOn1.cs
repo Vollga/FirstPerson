89b20c1a7215902d14d14a0cb664bb9e5c3e27c3
Vollga <36159821+Vollga@users.noreply.github.com>
2018-03-30 22:13:18 +0200

    a

---
+++ BPW First Person/Assets/SteppedOn1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteppedOn1 : MonoBehaviour {
+
+	public GameObject door;
+	public GameObject[] lines;
+	public Material colourOn;
+
+	MeshRenderer drend;
+	BoxCollider collision;
+	MeshRenderer[] lrend;
+
+
+	void Start () {
+		drend = door.GetComponent <MeshRenderer> ();
+		collision = door.GetComponent <BoxCollider> ();
+		lrend = new MeshRenderer[lines.Length];
+		for (int i = 0; i < lines.Length; i++) 
+		{
+			lrend[i] = lines[i].GetComponent <MeshRenderer> ();
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		drend.enabled = false;
+		collision.enabled = false;
+		for (int i = 0; i < lrend.Length; i++) 
+		{
+			lrend[i].material = colourOn;
+		}
+	}
+}
---
+++ BPW First Person/Assets/SteppedOn2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteppedOn2 : MonoBehaviour {
+
+	public GameObject[] Trapdoors;
+	public GameObject[] lines;
+	public Material colourOn;
+
+	Rigidbody[] rigid;
+	MeshRenderer[] lrend;
+
+
+	void Start () {
+		rigid = new Rigidbody[Trapdoors.Length];
+		for (int i = 0; i < Trapdoors.Length; i++)
+		{
+			rigid[i] = Trapdoors [i].GetComponent <Rigidbody> ();
+		}
+		lrend = new MeshRenderer[lines.Length];
+		for (int i = 0; i < lines.Length; i++) 
+		{
+			lrend[i] = lines[i].GetComponent <MeshRenderer> ();
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		for (int i = 0; i < rigid.Length; i++)
+		{
+			rigid [i].freezeRotation = false;
+		}
+		for (int i = 0; i < lrend.Length; i++) 
+		{
+			lrend[i].material = colourOn;
+		}
+	}
+}
